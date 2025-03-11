a518fe7a846c7557d8705e349d91f853873789c9
aitlor <aitalolor@alu.edu.gva.es>
2025-03-11 15:54:00 +0100

    HTML/JS/CSS Register Updated

--- register/app.js
+++ register/app.js
@@ -1,1 +1,26 @@
-console.log('Hola')
+document
+  .getElementById('registerForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    // Validar email
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    if (!emailPattern.test(email)) {
+      alert('Por favor, introduce un email válido.')
+      return
+    }
+
+    // Validar contraseña (mínimo 8 caracteres, una letra mayúscula, una minúscula y un número)
+    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/
+    if (!passwordPattern.test(password)) {
+      alert(
+        'La contraseña debe tener al menos 8 caracteres, incluyendo una letra mayúscula, una letra minúscula y un número.'
+      )
+      return
+    }
+
+    alert('Registro exitoso')
+  })
